1d3cf2ed8b47cfcdddb10f487a26d7142928939b
Kiran <kkp792001@gmail.com>
2023-03-12 21:19:19 +0530

    working on Experience page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css'
 import Navbar from './Component/Navbar/Navbar';
 import Intro from './Component/Intro/Intro';
 import Services from './Component/Services/Services';
+import Experience from './Component/Experience/Experience';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
-      <Services/>
+      <Services />
+      <Experience />
     </div>
   );
 }
