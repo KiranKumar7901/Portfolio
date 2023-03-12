0b6ca95c6bdfaa3f708b233f7db6feffa4af1600
Kiran <kkp792001@gmail.com>
2023-03-12 21:25:05 +0530

    Working on work page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Component/Navbar/Navbar';
 import Intro from './Component/Intro/Intro';
 import Services from './Component/Services/Services';
 import Experience from './Component/Experience/Experience';
+import Work from './Component/Work/Work';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Intro />
       <Services />
       <Experience />
+      <Work />
     </div>
   );
 }
