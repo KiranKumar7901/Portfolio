6470afbacae64ac170488685b68ee1bc68a9d9a0
Kiran <kkp792001@gmail.com>
2023-03-14 14:54:12 +0530

    implementing dark mode

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ import Portfolio from './Component/Portfolio/Portfolio';
 import Testimonials from './Component/Testimonials/Testimonials';
 import Contact from './Component/Contact/Contact';
 import Footer from './Component/Footer/Footer';
+import { themeContext } from './context';
+import { useContext } from 'react';
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
+    <div className="App"
+      style={{
+        background: darkMode ? 'black' : '',
+      color: darkMode? 'white':''}}>
       <Navbar />
       <Intro />
       <Services />
--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Footer.css'
+import wave from '../Images/wave.png'
+import Instagram from '@iconscout/react-unicons/icons/uil-instagram'
+import FaceBook from '@iconscout/react-unicons/icons/uil-facebook'
+import GitHub from '@iconscout/react-unicons/icons/uil-github'
 
 const Footer = () => {
   return (
       <div className="footer">
-          <img src="" alt="" /></div>
+      <img src={wave} alt="" style={{ width: '100%'}} />
+      <div className="f-content">
+        <span>kirankumar992001@gmail.com</span>
+        <div className="f-icons">
+          <Instagram color='white' size='3rem'/>
+          <FaceBook color='white' size='3rem'/>
+          <GitHub color='white' size='3rem'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
