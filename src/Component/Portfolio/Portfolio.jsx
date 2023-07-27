0b6594f45edb3ed7cc5f338e09eeeac5aedb6c1b
Kiran <kkp792001@gmail.com>
2023-07-27 14:51:27 +0530

    adding projects

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import './Portfolio.css'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-
 // import th1 from "../Images/th1.jpg";
 import th1 from "../Images/th2.jpg";
 import th2 from "../Images/th2.jpg";
@@ -10,6 +7,13 @@ import th3 from "../Images/th3.jpg";
 
 import { themeContext } from '../../context';
 import { useContext } from 'react';
+
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+SwiperCore.use([EffectCoverflow, Pagination]);
 
 const Portfolio = () => {
     const theme = useContext(themeContext);
@@ -22,9 +26,18 @@ const Portfolio = () => {
             <span>Portfolio</span>
             {/* Slider */}
             <Swiper
-                spaceBetween={240}
-                slidesPerView={1}
+                effect={"coverflow"}
                 grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                coverflowEffect={{
+                  rotate: 1,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: false,
+                }}
+                pagination={true}
                 className='portfolio-slider'
             >
                 <SwiperSlide>
@@ -46,14 +59,9 @@ const Portfolio = () => {
                     <img src={th3} alt="" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={th1} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={th2} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
                     <img src={th3} alt="" />
                 </SwiperSlide>
+                
             </Swiper>
             <div className="blur p-blur1" style={{background: 'var(--purple)'}}></div>
             <div className="blur p-blur2" style={{background: '#fca71fab'}}></div>
