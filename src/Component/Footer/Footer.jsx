d4ea1300f2f4fe91f7ef273c3de0cec0db16b89a
Kiran <kkp792001@gmail.com>
2023-07-29 23:46:48 +0530

    color correction

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -19,9 +19,9 @@ const Footer = () => {
         <span> <a href="mailto:kirankumar992001@gmail.com" style={darkMode?{color: 'black'}:{}}>kirankumar992001@gmail.com</a></span>
         <span><a href="tel:+917906533864" style={darkMode?{color: 'black'}:{}}>+91-7906533864</a></span>
         <div className="f-icons">
-          <a href="https://www.linkedin.com/in/kiran-kumar-514793179"><LinkedIn size='3rem' style={darkMode?{color:'black'}:{color: 'white'}}/></a>
-          <a href="https://github.com/KiranKumar7901"><GitHub style={darkMode?{color:'black'}:{color: 'white'}} size='3rem'/></a>
-          <a href="https://instagram.com/_karanprajapati_01?utm_source=qr&igshid=MzNINGNkZWQ4Mg%3D%3d"><Instagram style={darkMode?{color:'black'}:{color: 'white'}} size='3rem'/></a>
+          <a href="https://www.linkedin.com/in/kiran-kumar-514793179"><LinkedIn size='3rem' style={darkMode?{color:'black'}:{}}/></a>
+          <a href="https://github.com/KiranKumar7901"><GitHub style={darkMode?{color:'black'}:{}} size='3rem'/></a>
+          <a href="https://instagram.com/_karanprajapati_01?utm_source=qr&igshid=MzNINGNkZWQ4Mg%3D%3d"><Instagram style={darkMode?{color:'black'}:{}} size='3rem'/></a>
         </div>
       </div>
     </div>
